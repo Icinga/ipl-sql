efd5c42716329c8cd426eea7117c95c9e222478a
Martin Neubert <martin.neubert@t-systems.com>
2019-06-05 12:45:53 +0200

    Added SSL Options for MySQL Adapter
    fixes #9

--- src/Adapter/Mysql.php
+++ src/Adapter/Mysql.php
@@ -3,6 +3,8 @@
 namespace ipl\Sql\Adapter;
 
 use ipl\Sql\Connection;
+use ipl\Sql\Config;
+use PDO;
 
 class Mysql extends BaseAdapter
 {
@@ -15,5 +17,34 @@ class Mysql extends BaseAdapter
         $db->prepexec('SET time_zone = ?', [$this->getTimezoneOffset()]);
 
         return $this;
+    }
+
+    public function getOptions(Config $config)
+    {
+        $options = parent::getOptions($config);
+
+        if (isset($config->use_ssl) && $config->use_ssl === '1') {
+            if (isset($config->ssl_key)) {
+                $options[PDO::MYSQL_ATTR_SSL_KEY] = $config->ssl_key;
+            }
+
+            if (isset($config->ssl_cert)) {
+                $options[PDO::MYSQL_ATTR_SSL_CERT] = $config->ssl_cert;
+            }
+
+            if (isset($config->ssl_ca)) {
+                $options[PDO::MYSQL_ATTR_SSL_CA] = $config->ssl_ca;
+            }
+
+            if (isset($config->ssl_capath)) {
+                $options[PDO::MYSQL_ATTR_SSL_CAPATH] = $config->ssl_capath;
+            }
+
+            if (isset($config->ssl_cipher)) {
+                $options[PDO::MYSQL_ATTR_SSL_CIPHER] = $config->ssl_cipher;
+            }
+        }
+
+        return $options;
     }
 }
