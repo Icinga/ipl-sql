90317a2d5ad9c74c9be596e19fc7ae7ff9c4df66
Eric Lippmann <eric.lippmann@icinga.com>
2020-07-06 16:13:36 +0200

    Refine docs for the supported where styles

--- src/WhereInterface.php
+++ src/WhereInterface.php
@@ -18,8 +18,8 @@ interface WhereInterface
      * Add a WHERE part of the query
      *
      * This method lets you specify the WHERE part of the query using one of the two following supported formats:
-     * * String format, e.g. 'id = 1'
-     * * Array format, e.g. ['id = ?' => 1, ...]
+     * * String format, e.g. 'id = 1', i.e. `where(string $condition [, mixed ...$args])`
+     * * Array format, e.g. ['id = ?' => 1, ...], i.e. `where(array $condition [, string $operator])`
      *
      * This method does NOT quote the columns you specify for the WHERE.
      * If you allow user input here, you must protected yourself against SQL injection using
@@ -31,9 +31,9 @@ interface WhereInterface
      * the specified WHERE part using the AND operator.
      *
      * @param string|ExpressionInterface|Select|array $condition The WHERE condition
-     * @param mixed  $args  When condition is a string, this allows to pass parameter values for eventual placeholders
-     *                      When condition is an array, the only allowed parameter is the operator that should be used
-     *                      to combine those conditions. This operator defaults to Sql::ALL (AND)
+     * @param mixed $args If condition is a string, parameter values for placeholders in the condition can be passed.
+     *                    If condition is an array, the only argument that is allowed is the operator to use to combine
+     *                    these conditions. By default, this operator is {@link Sql::ALL} (AND)
      *
      * @return $this
      */
