2e1ec53c9cf687a123d41376c207f828378f3dc6
Eric Lippmann <eric.lippmann@icinga.com>
2021-07-19 13:58:11 +0200

    Don't accept empty strings for MySQL SSL attributes

--- src/Adapter/Mysql.php
+++ src/Adapter/Mysql.php
@@ -23,24 +23,24 @@ class Mysql extends BaseAdapter
     {
         $options = parent::getOptions($config);
 
-        if (isset($config->use_ssl) && $config->use_ssl === '1') {
-            if (isset($config->ssl_key)) {
+        if (! empty($config->use_ssl)) {
+            if (! empty($config->ssl_key)) {
                 $options[PDO::MYSQL_ATTR_SSL_KEY] = $config->ssl_key;
             }
 
-            if (isset($config->ssl_cert)) {
+            if (! empty($config->ssl_cert)) {
                 $options[PDO::MYSQL_ATTR_SSL_CERT] = $config->ssl_cert;
             }
 
-            if (isset($config->ssl_ca)) {
+            if (! empty($config->ssl_ca)) {
                 $options[PDO::MYSQL_ATTR_SSL_CA] = $config->ssl_ca;
             }
 
-            if (isset($config->ssl_capath)) {
+            if (! empty($config->ssl_capath)) {
                 $options[PDO::MYSQL_ATTR_SSL_CAPATH] = $config->ssl_capath;
             }
 
-            if (isset($config->ssl_cipher)) {
+            if (! empty($config->ssl_cipher)) {
                 $options[PDO::MYSQL_ATTR_SSL_CIPHER] = $config->ssl_cipher;
             }
 
