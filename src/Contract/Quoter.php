ce0725c01c8be8b0af2286b6aae0e8435c98194e
Johannes Meyer <johannes.meyer@icinga.com>
2022-02-21 14:04:47 +0100

    Quoter: Accept arrays of strings and quote parts as-is

--- src/Contract/Quoter.php
+++ src/Contract/Quoter.php
@@ -5,13 +5,17 @@ namespace ipl\Sql\Contract;
 interface Quoter
 {
     /**
-     * Quote a string so that it can be safely used as table or column name, even if it is a reserved name
+     * Quote an identifier so that it can be safely used as table or column name, even if it is a reserved name
+     *
+     * If a string is passed that contains dots, the parts separated by them are quoted individually.
+     * (e.g. `myschema.mytable` turns into `"myschema"."mytable"`) If an array is passed, the entries
+     * are quoted as-is. (e.g. `[myschema.my, table]` turns into `"myschema.my"."table"`)
      *
      * The quote character depends on the underlying database adapter that is being used.
      *
-     * @param string $identifier
+     * @param string|string[] $identifiers
      *
      * @return string
      */
-    public function quoteIdentifier($identifier);
+    public function quoteIdentifier($identifiers);
 }
