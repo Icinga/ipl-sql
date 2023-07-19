a6ae9373c7049a4c0000fffe5304f379ea45ad6a
Eric Lippmann <eric.lippmann@icinga.com>
2023-07-19 13:01:28 +0200

    Config: Move class properties above methods

--- src/Config.php
+++ src/Config.php
@@ -11,28 +11,6 @@ use function ipl\Stdlib\get_php_type;
  */
 class Config
 {
-    /**
-     * Create a new SQL connection configuration from the given configuration key-value pairs
-     *
-     * @param iterable $config Configuration key-value pairs
-     *
-     * @throws InvalidArgumentException If $config is not iterable
-     */
-    public function __construct($config)
-    {
-        if (! is_iterable($config)) {
-            throw new InvalidArgumentException(sprintf(
-                '%s expects parameter one to be iterable, got %s instead',
-                __METHOD__,
-                get_php_type($config)
-            ));
-        }
-
-        foreach ($config as $key => $value) {
-            $this->$key = $value;
-        }
-    }
-
     /** @var string Type of the DBMS */
     public $db;
 
@@ -62,11 +40,32 @@ class Config
 
     /**
      * PDO connect options
-     *
      * Array of key-value pairs that should be set when calling {@link Connection::connect()} in order to establish a DB
      * connection.
      *
      * @var array
      */
     public $options;
+
+    /**
+     * Create a new SQL connection configuration from the given configuration key-value pairs
+     *
+     * @param iterable $config Configuration key-value pairs
+     *
+     * @throws InvalidArgumentException If $config is not iterable
+     */
+    public function __construct($config)
+    {
+        if (! is_iterable($config)) {
+            throw new InvalidArgumentException(sprintf(
+                '%s expects parameter one to be iterable, got %s instead',
+                __METHOD__,
+                get_php_type($config)
+            ));
+        }
+
+        foreach ($config as $key => $value) {
+            $this->$key = $value;
+        }
+    }
 }
