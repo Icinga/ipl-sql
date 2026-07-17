684152a3acd06a9c72a14c43b611273207e1d3e3
Johannes Meyer <johannes.meyer@icinga.com>
2026-07-17 17:46:24 +0200

    SharedDatabases: Initiate only a single connection per database
    Since support for PHPUnit 10 was added, the data provider is being
    evaluated per class usage and only once per run, meaning that the
    setup and tear down routines for the schema are processed multiple
    times. This on its own isn't a problem, but test fixtures will add
    up during execution of multiple independent test cases and this
    wasn't intended when this trait was introduced.
    
    This change introduces a specific singleton to maintain a global
    state across different test cases in order to have only a single
    connection per database. It also orchestrates schema handling
    better since it will be done per test case again now and is even
    adjustable to support shared state between different test cases.
    Each transaction can also be put into a database transaction
    which is rolled back automatically to ensure that no state, that
    is not part of the schema set-up, influences subsequent tests.
    
    Also drops the following methods that were not marked internal:
    - getEnvironmentVariable
    - getConnectionConfig

--- tests/SharedDatabasesTest.php
+++ tests/SharedDatabasesTest.php
@@ -6,65 +6,103 @@ use ipl\Sql\Connection;
 use ipl\Sql\Select;
 use ipl\Sql\Test\SharedDatabases;
 use ipl\Sql\Test\TestCase;
+use PDO;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Depends;
 
 /**
  * A test for a test component! Yay!
  */
+#[SharedDatabases\TransactionIsolation]
 class SharedDatabasesTest extends TestCase
 {
     use SharedDatabases;
 
     #[DataProvider('sharedDatabases')]
+    public function testSharedDatabasesGroup(Connection $db): void
+    {
+        $this->assertNotNull(
+            $db->select(
+                (new Select())
+                    ->columns('name')
+                    ->from('__test_group')
+            )->fetchColumn(),
+            'No database group has been established.'
+        );
+    }
+
+    #[DataProvider('sharedDatabases')]
     public function testInsert(Connection $db)
     {
-        // This is the first case, so the table must have been dropped and be empty
-        $result = $db->select((new Select())->columns('name')->from('test'))->fetchAll();
-        $this->assertEmpty($result);
-
         $db->insert('test', ['name' => 'test']);
         $db->insert('test', ['name' => 'test2']);
+
+        $result = $db->select(
+            (new Select())
+                ->columns('name')
+                ->from('test')
+        )->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertSame(
+            [['name' => 'test'], ['name' => 'test2']],
+            $result
+        );
     }
 
     #[Depends('testInsert')]
     #[DataProvider('sharedDatabases')]
     public function testSelect(Connection $db)
     {
-        // The previous case inserts "name=test" but tearDown removes it
-        $result = $db->select((new Select())->columns('name')->from('test'))->fetchAll();
+        $db->insert('test', ['name' => 'test']);
+
+        $result = $db->select(
+            (new Select())
+                ->columns('name')
+                ->from('test')
+        )->fetchAll(PDO::FETCH_ASSOC);
+
         $this->assertCount(1, $result);
-        $this->assertSame('test2', $result[0]['name']);
+        $this->assertSame('test', $result[0]['name']);
     }
 
     #[Depends('testSelect')]
     #[DataProvider('sharedDatabases')]
     public function testUpdate(Connection $db)
     {
+        $db->insert('test', ['name' => 'test']);
+        $db->insert('test', ['name' => 'test2']);
+
         $stmt = $db->update('test', ['name' => 'test3'], ['name = ?' => 'test2']);
         $this->assertEquals(1, $stmt->rowCount());
+
+        $result = $db->select(
+            (new Select())
+                ->columns('name')
+                ->from('test')
+        )->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertSame(
+            [['name' => 'test'], ['name' => 'test3']],
+            $result
+        );
     }
 
-    #[Depends('testUpdate')]
+    #[Depends('testInsert')]
     #[DataProvider('sharedDatabases')]
     public function testDelete(Connection $db)
     {
-        $stmt = $db->delete('test', ['name = ?' => 'test3']);
+        $db->insert('test', ['name' => 'test']);
+        $stmt = $db->delete('test', ['name = ?' => 'test']);
         $this->assertEquals(1, $stmt->rowCount());
     }
 
-    protected static function setUpSchema(Connection $db, string $driver): void
+    public static function setUpSchema(Connection $db, string $driver): void
     {
         $db->exec('CREATE TABLE test (name VARCHAR(255))');
     }
 
-    protected static function tearDownSchema(Connection $db, string $driver): void
+    public static function tearDownSchema(Connection $db, string $driver): void
     {
         $db->exec('DROP TABLE IF EXISTS test');
-    }
-
-    public function tearDown(): void
-    {
-        $this->getConnection()->delete('test', ['name = ?' => ['test']]);
     }
 }
